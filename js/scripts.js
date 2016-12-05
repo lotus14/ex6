50e83a8d0060812fc156a69c6b96905bcfddd2ef
lotus14 <josiegeng@gmail.com>
2016-12-05 00:22:41 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(function() {
+	var $grid = $( '#tp-grid' ),
+		$name = $( '#name' ),
+		$close = $( '#close' ),
+		$loader = $( '<div class="loader"><i></i><i></i><i></i><i></i><i></i><i></i><span>Loading...</span></div>' ).insertBefore( $grid ),
+		stapel = $grid.stapel( {
+			onLoad : function() {
+				$loader.remove();
+			},
+			onBeforeOpen : function( pileName ) {
+				$name.html( pileName );
+			},
+			onAfterOpen : function( pileName ) {
+				$close.show();
+			}
+		} );
+
+	$close.on( 'click', function() {
+		$close.hide();
+		$name.empty();
+		stapel.closePile();
+	} );
+} );
